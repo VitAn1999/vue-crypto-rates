1b736dea679cf3c5fb2bd5ea07f9eeabb1cdc807
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-05-18 22:42:06 +0300

    create functions subscribeToTicker and unsubscribeFromTicker

--- src/api/loadRates.js
+++ src/api/loadRates.js
@@ -1,6 +1,8 @@
 const API_KEY =
   '39149bbaadcf44d2a07bf89388b77c90d10090763f617e635d62cda10bb16c04';
 
+const tickersHandler = new Map();
+
 export const loadRates = tickers =>
   fetch(
     `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tickers.join(
@@ -13,3 +15,16 @@ export const loadRates = tickers =>
         Object.entries(loadData).map(([key, value]) => [key, value.USD])
       )
     );
+
+export const subscribeToTicker = (ticker, cb) => {
+  const subscriber = tickersHandler.get(ticker) || [];
+  tickersHandler.set(ticker, [...subscriber, cb]);
+};
+
+export const unsubscribeFromTicker = (ticker, cb) => {
+  const subscriber = tickersHandler.get(ticker) || [];
+  tickersHandler.set(
+    ticker,
+    subscriber.filter(fn => fn !== cb)
+  );
+};
