cc973d4cf83fb5ddff43de20261574d63a23cd5d
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-05-19 23:54:29 +0300

    send to WebSocket

--- src/api/loadRates.js
+++ src/api/loadRates.js
@@ -8,47 +8,58 @@ const socket = new WebSocket(
   `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
 );
 
-socket.addEventListener('message', message => {
-  console.log(message);
-});
+const AGGREGATE_INDEX = '5';
 
-export const loadRates = () => {
-  if (tickersHandler.size === 0) {
+// при получении сообщения от WebSocket запускаем обработчик событий
+socket.addEventListener('message', message => {
+  // из message.data получаем type, tickerName и rate
+  const { TYPE: type, FROMSYMBOL: tickerName, PRICE: rate } = JSON.parse(
+    message.data
+  );
+  if (type !== AGGREGATE_INDEX) {
     return;
   }
-  fetch(
-    `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${Array.from(
-      tickersHandler.keys()
-    ).join(',')}&tsyms=USD&api_key=${API_KEY}`
-  )
-    .then(response => response.json())
-    .then(loadData => {
-      // в updateRates помещаем объект из ключа Наименование криптовалюты и
-      // значения Курс криптовалюты {'BTC': 51250.52}
-      const updatedRates = Object.fromEntries(
-        Object.entries(loadData).map(([key, value]) => [key, value.USD])
-      );
-      // Превращаем updateRates в массив ['BTC', 51250.52]
-      Object.entries(updatedRates).forEach(([tickerName, rate]) => {
-        // в массив handlers помещаем callback-функции по ключу tickerName
-        const handlers = tickersHandler.get(tickerName) || [];
-        // перебираем все колбэки и в качестве аргумента помещаем туда
-        // актуальные курсы
-        console.log(tickerName, rate);
-        handlers.forEach(cb => cb(rate));
-      });
-    });
-};
+  // в массив handlers помещаем callback-функции по ключу tickerName
+  const handlers = tickersHandler.get(tickerName) || [];
+  // перебираем все колбэки и в качестве аргумента помещаем туда
+  // актуальные курсы
+  handlers.forEach(cb => cb(rate));
+});
 
-function subscribeToTickerWithWS(ticker) {
-  const message = JSON.stringify({
-    action: 'SubAdd',
-    subs: [`5~CCCAGG~${ticker}~USD`]
-  });
+// export const loadRates = () => {
+//   if (tickersHandler.size === 0) {
+//     return;
+//   }
+//   fetch(
+//     `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${Array.from(
+//       tickersHandler.keys()
+//     ).join(',')}&tsyms=USD&api_key=${API_KEY}`
+//   )
+//     .then(response => response.json())
+//     .then(loadData => {
+//       // в updateRates помещаем объект из ключа Наименование криптовалюты и
+//       // значения Курс криптовалюты {'BTC': 51250.52}
+//       const updatedRates = Object.fromEntries(
+//         Object.entries(loadData).map(([key, value]) => [key, value.USD])
+//       );
+//       // Превращаем updateRates в массив ['BTC', 51250.52]
+//       Object.entries(updatedRates).forEach(([tickerName, rate]) => {
+//         // в массив handlers помещаем callback-функции по ключу tickerName
+//         const handlers = tickersHandler.get(tickerName) || [];
+//         // перебираем все колбэки и в качестве аргумента помещаем туда
+//         // актуальные курсы
+//         console.log(tickerName, rate);
+//         handlers.forEach(cb => cb(rate));
+//       });
+//     });
+// };
+
+function sendToWebSocket(message) {
   if (socket.readyState === WebSocket.OPEN) {
     socket.send(message);
     return;
   }
+  // если не открыт, открываем сокер и отправляем сообщение
   socket.addEventListener(
     'open',
     () => {
@@ -56,6 +67,24 @@ function subscribeToTickerWithWS(ticker) {
     },
     { once: true }
   );
+}
+
+function subscribeToTickerWithWS(ticker) {
+  // сообщение с подпиской отправляемое на сервер
+  const message = JSON.stringify({
+    action: 'SubAdd',
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+  sendToWebSocket(message);
+}
+
+function unsubscribeFromTickerWithWS(ticker) {
+  // сообщение с отпиской отправляемое на сервер
+  const message = JSON.stringify({
+    action: 'SubRemove',
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+  sendToWebSocket(message);
 }
 
 // функция подписка на тикер, добавляет в мапу tickersHandler по ключу тикера
@@ -68,8 +97,7 @@ export const subscribeToTicker = (ticker, cb) => {
 
 export const unsubscribeFromTicker = ticker => {
   tickersHandler.delete(ticker);
+  unsubscribeFromTickerWithWS(ticker);
 };
-
-setInterval(loadRates, 5000);
 
 window.tickers = tickersHandler;
